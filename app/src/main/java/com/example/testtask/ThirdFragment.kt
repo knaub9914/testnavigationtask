cc2b55303775dd619dd12dc54db4ba74d9f2f365
knaub9914 <constantine.dfb@gmail.com>
2023-11-20 20:41:09 +0700

    final and first commit

---
+++ app/src/main/java/com/example/testtask/ThirdFragment.kt
@@ -0,0 +1,38 @@
+package com.example.testtask
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.testtask.databinding.FragmentFirstBinding
+import com.example.testtask.databinding.FragmentThirdBinding
+
+class ThirdFragment : Fragment() {
+
+    private var _binding: FragmentThirdBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentThirdBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.button.setOnClickListener {
+            findNavController().navigate(R.id.action_thirdFragment_to_firstFragment)
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
